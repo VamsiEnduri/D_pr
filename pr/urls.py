6027b7d6131165b1ca4f25d814a867e9c7b0c0da
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2025-11-28 06:54:51 +0530

    get

--- pr/urls.py
+++ pr/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import e,createEmp,getAllEmps
 urlpatterns=[
     path("api/employees/add/",e,name="v"),
-    path("api/employees/get-all-emps",getAllEmps,name="getAllEmps")
+    path("api/employees/get-all-emps",getAllEmps,name="getAllEmps"),
     #  path('api/employees/get-all-emps/', getAllEmps, name='getAllEmps'),
     path('api/employees/create/', createEmp, name='createEmp'),
 ]
